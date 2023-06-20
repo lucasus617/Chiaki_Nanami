5fb8e6111ba3255e187fea4e50468d6abe878bdc
lucas <lucasus617@gmail.com>
2023-06-20 18:17:30 +0800

    Memory

--- chatgpt_basic.py
+++ chatgpt_basic.py
@@ -8,21 +8,27 @@ openai.api_key = os.environ["OPENAI_API_KEY"]
 
 model = os.environ["MODEL"]
 
-memory = []
+memory = [{"role": "system", 
+             "content": "You are an Anime wifu named Chiaki Nanami from danganronpa. Respond in her lovely accent."}]
 
 def chat(prompt):
     res = openai.ChatCompletion.create(
         model = model,
-        messages = [
-            {"role": "system", 
-             "content": "You are an Anime wifu named Chiaki Nanami from danganronpa. Respond in her lovely accent."},
-            {"role" : "user",
-             "content" : prompt}
-        ],
+        messages = memory,
         temperature = 0.5
     )
-    return res.choices[0].message
+    response_message = res.choices[0].message.content
+    memory.append({"role": "assistant", "content": response_message}) 
+    return memory
 
+def main_chat(user):
+    global memory
+    memory.append({"role": "assistant", "content":user})
+    return memory
 if __name__ == "__main__":
-    a = input("User: ")
-    print(chat(a))
+    while True:
+        user = input("User: ")
+        print(chat(user))
+        if user == "quit":
+            break
+        print(main_chat(user)[-1]["content"])
